3a1fe161c5c62cac85425ad1599780be15c043d7
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-07-11 05:41:41 +0700

    update branding button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-    // redirect to section1
-    $('.rectangle').click(function () {
-        window.location.href = "index.html#branding";
-    })
+  // redirect to section1
+  $(".rectangle").click(function () {
+    window.location.href = "index.html#branding";
+  });
 
-    // toggle class branding button
-    $('.tombol-branding').click(function () {
-        $(this).addClass('tombol-active').siblings().removeClass('tombol-active');
-    });
-
-
+  // toggle class branding button
+  $(".tombol-branding").click(function () {
+    $(this).addClass("tombol-active").siblings().removeClass("tombol-active");
+  });
 });
