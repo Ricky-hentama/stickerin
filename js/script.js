728b454a73470cc95b436c6e473c7ebcc79fc1da
Ricky Hendra Pratama <60565163+Ricky-hentama@users.noreply.github.com>
2020-07-11 21:41:39 +0700

    update card

--- js/script.js
+++ js/script.js
@@ -8,4 +8,23 @@ $(document).ready(function () {
   $(".tombol-branding").click(function () {
     $(this).addClass("tombol-active").siblings().removeClass("tombol-active");
   });
+  // responsive card
+  var layar = $(window).width();
+  if (layar > 750) {
+    jumlahCard = 4;
+  } else if (layar < 750 && layar > 550) {
+    jumlahCard = 3;
+  } else {
+    jumlahCard = 2;
+  }
+  var jumlahCard;
+  var swiper = new Swiper('.swiper-container', {
+
+    slidesPerView: jumlahCard,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })
 });
