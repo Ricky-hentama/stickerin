e838d138fa797feda21bb679d6da76aedfd7032b
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-08-14 11:38:48 +0700

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,73 +1,71 @@
-// !(function ($) {
-//   "use strict";
-//   // toggle class branding button
-//   $(".tombol-branding").click(function () {
-//     $(this).addClass("tombol-active").siblings().removeClass("tombol-active");
-//   });
-//   // Porfolio isotope and filter
-//   $(window).on("load", function () {
-//     var portfolioIsotope = $(".portfolio-container").isotope({
-//       itemSelector: ".portfolio-item",
-//       layoutMode: "fitRows",
-//     });
+!(function ($) {
+  "use strict";
+  // toggle class branding button
+  $(".tombol-branding").click(function () {
+    $(this).addClass("tombol-active").siblings().removeClass("tombol-active");
+  });
+  // Porfolio isotope and filter
+  $(window).on("load", function () {
+    var portfolioIsotope = $(".portfolio-container").isotope({
+      itemSelector: ".portfolio-item",
+      layoutMode: "fitRows",
+    });
 
-//     $("#portfolio-flters li").on("click", function () {
-//       $("#portfolio-flters li").removeClass("filter-active");
-//       $(this).addClass("filter-active");
-//       portfolioIsotope.isotope({
-//         filter: $(this).data("filter"),
-//       });
-//     });
+    $("#portfolio-flters li").on("click", function () {
+      $("#portfolio-flters li").removeClass("filter-active");
+      $(this).addClass("filter-active");
+      portfolioIsotope.isotope({
+        filter: $(this).data("filter"),
+      });
+    });
 
-//     // Initiate venobox (lightbox feature used in portofilo)
-//     $(document).ready(function () {
-//       $(".venobox").venobox();
-//     });
-//   });
-// })(jQuery);
+    // Initiate venobox (lightbox feature used in portofilo)
+
+  });
+})(jQuery);
 
 // animasi
 
-$(".tombol-branding").click(function (e) {
-  $(this).addClass("tombol-active").siblings().removeClass("tombol-active");
+// $(".tombol-branding").click(function (e) {
+//   $(this).addClass("tombol-active").siblings().removeClass("tombol-active");
 
-  if (e.target.dataset.target == "portfolio-branding-mobil") {
-    $(".portfolio-branding-mobil")
-      .addClass("portfolio-show")
-      .siblings()
-      .removeClass("portfolio-show");
-  }
-  if (e.target.dataset.target == "portfolio-branding-kantor") {
-    $(".portfolio-branding-kantor")
-      .addClass("portfolio-show")
-      .siblings()
-      .removeClass("portfolio-show");
-  }
-  if (e.target.dataset.target == " portfolio-digital-printing") {
-    $(".portfolio-digital-printing")
-      .addClass("portfolio-show")
-      .siblings()
-      .removeClass("portfolio-show");
-  }
-  if (e.target.dataset.target == " portfolio-cutting-sticker") {
-    $(".portfolio-cutting-sticker")
-      .addClass("portfolio-show")
-      .siblings()
-      .removeClass("portfolio-show");
-  }
-  if (e.target.dataset.target == " portfolio-stock-sticker") {
-    $(".portfolio-stock-sticker")
-      .addClass("portfolio-show")
-      .siblings()
-      .removeClass("portfolio-show");
-  }
-});
+//   if (e.target.dataset.target == "portfolio-branding-mobil") {
+//     $(".portfolio-branding-mobil")
+//       .addClass("portfolio-show")
+//       .siblings()
+//       .removeClass("portfolio-show");
+//   }
+//   if (e.target.dataset.target == "portfolio-branding-kantor") {
+//     $(".portfolio-branding-kantor")
+//       .addClass("portfolio-show")
+//       .siblings()
+//       .removeClass("portfolio-show");
+//   }
+//   if (e.target.dataset.target == " portfolio-digital-printing") {
+//     $(".portfolio-digital-printing")
+//       .addClass("portfolio-show")
+//       .siblings()
+//       .removeClass("portfolio-show");
+//   }
+//   if (e.target.dataset.target == " portfolio-cutting-sticker") {
+//     $(".portfolio-cutting-sticker")
+//       .addClass("portfolio-show")
+//       .siblings()
+//       .removeClass("portfolio-show");
+//   }
+//   if (e.target.dataset.target == " portfolio-stock-sticker") {
+//     $(".portfolio-stock-sticker")
+//       .addClass("portfolio-show")
+//       .siblings()
+//       .removeClass("portfolio-show");
+//   }
+// });
 
-$(window).on("load", function () {
-  // portfolio
-  $(".portfolio-img").each(function (i) {
-    setTimeout(function () {
-      $(".portfolio-img").eq(i).addClass("element-show");
-    }, 200 * (i + 1));
-  });
-});
+// $(window).on("load", function () {
+//   // portfolio
+//   $(".portfolio-img").each(function (i) {
+//     setTimeout(function () {
+//       $(".portfolio-img").eq(i).addClass("element-show");
+//     }, 200 * (i + 1));
+//   });
+// });
